18b2c80ec1c1316e63860c8e1b4fb566deaa0868
jazzytoor <32848916+jazzytoor@users.noreply.github.com>
2026-01-04 15:19:21 +0000

    chore: lets try create some resources

--- live/spoke/iam.tf
+++ live/spoke/iam.tf
@@ -1,18 +1,18 @@
-module "ado_custom_policy" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
+# module "ado_custom_policy" {
+#   source = "terraform-aws-modules/iam/aws//modules/iam-policy"
 
-  name = "ado"
+#   name = "ado"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:ListAllMyBuckets"
-        ]
-        Resource = ["*"]
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "s3:ListAllMyBuckets"
+#         ]
+#         Resource = ["*"]
+#       }
+#     ]
+#   })
+# }
